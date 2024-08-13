87d1575298df2281bbf257b0604ca7e7bd7efb53
samtasticc <samantha.henesy@gmail.com>
2024-08-12 22:02:42 -0400

    step 1 pass

--- js/app.js
+++ js/app.js
@@ -3,8 +3,13 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
+let state = {
+    boredom: 0,
+    hunger: 0,
+    sleepiness: 0,
+}
 
-
+let timer, gameOver; // this will assign the timer and gameOver a value of undefined
 
 /*------------------------ Cached Element References ------------------------*/
 
